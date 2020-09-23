873e03490ba9cde288f229ff85101bd9f5ee6906
ankitkrish <ankit.shrivastava@krishtechnolabs.com>
2020-09-23 20:16:19 +0530

    readme file

---
+++ app/code/TrainingAnkit/CustomerAccount/Block/Form/Login.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * TrainingAnkit
+ * Copyright (C) 2019 TrainingAnkit <info@magentocoders.com>
+ *
+ * NOTICE OF LICENSE
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://opensource.org/licenses/gpl-3.0.html.
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    TrainingAnkit
+ * @package     TrainingAnkit_CustomerAccount
+ * @copyright   Copyright (c) 2012 MagentoCoders (http://www.magentocoders.com/)
+ * @license     http://www.TrainingAnkit.com/license-agreement.html
+ * @author      MagentoCoders <info@magentocoders.com>
+ */
+
+namespace TrainingAnkit\CustomerAccount\Block\Form;
+
+use Magento\Customer\Model\Context;
+use Magento\Customer\Model\Form;
+use Magento\Customer\Model\Registration;
+use Magento\Customer\Model\Session;
+use Magento\Framework\View\Element\Template;
+use Magento\Store\Model\ScopeInterface;
+
+/**
+ * Class Login
+ * @package TrainingAnkit\CustomerAccount\Block\Form
+ */
+class Login extends Template
+{
+    /**
+     * @var Session
+     */
+    protected $customerSession;
+
+    /**
+     * @var \Magento\Framework\App\Http\Context
+     */
+    protected $httpContext;
+
+    /**
+     * Registration
+     *
+     * @var Registration
+     */
+    protected $registration;
+    /**
+     * @var \Magento\Framework\View\Element\Template\Context
+     */
+    private $context;
+    /**
+     * @var array
+     */
+    private $data;
+
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param Session $customerSession
+     * @param \Magento\Framework\App\Http\Context $httpContext
+     * @param Registration $registration
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        Session $customerSession,
+        \Magento\Framework\App\Http\Context $httpContext,
+        Registration $registration,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->customerSession = $customerSession;
+        $this->httpContext = $httpContext;
+        $this->registration = $registration;
+        $this->context = $context;
+        $this->data = $data;
+    }
+
+    /**
+     * Return registration
+     *
+     * @return Registration
+     */
+    public function getRegistration()
+    {
+        return $this->registration;
+    }
+
+    /**
+     * Retrieve form posting url
+     *
+     * @return string
+     */
+    public function getPostActionUrl()
+    {
+        return $this->getUrl('customer/Ajax/login');
+    }
+
+    /**
+     * Check if autocomplete is disabled on storefront
+     *
+     * @return bool
+     */
+    public function isAutocompleteDisabled()
+    {
+        return (bool)!$this->_scopeConfig->getValue(
+            Form::XML_PATH_ENABLE_AUTOCOMPLETE,
+            ScopeInterface::SCOPE_STORE
+        );
+    }
+
+    /**
+     * Checking customer login status
+     *
+     * @return bool
+     */
+    public function customerIsAlreadyLoggedIn()
+    {
+        return (bool)$this->httpContext->getValue(Context::CONTEXT_AUTH);
+    }
+
+    /**
+     * Retrieve registering URL
+     *
+     * @return string
+     */
+    public function getCustomerRegistrationUrl()
+    {
+        return $this->getUrl('customer/account/create');
+    }
+}
---
+++ app/code/TrainingAnkit/CustomerAccount/view/frontend/web/js/action/customer-authentication-popup.js
@@ -0,0 +1,151 @@
+define([
+    'jquery',
+    'Magento_Ui/js/modal/modal',
+    'Magento_Customer/js/customer-data',
+    'mage/storage',
+    'mage/translate',
+    'mage/mage',
+    'jquery/ui'
+], function ($, modal, customerData, storage, $t) {
+    'use strict';
+
+    $.widget('trainingankit.customerAuthenticationPopup', {
+        options: {
+            login: '#customer-popup-login',
+            nextRegister: '#customer-popup-registration',
+            register: '#customer-popup-register',
+            prevLogin: '#customer-popup-sign-in'
+        },
+
+        /**
+         *
+         * @private
+         */
+        _create: function () {
+            var self = this,
+                authentication_options = {
+                    type: 'popup',
+                    responsive: true,
+                    innerScroll: true,
+                    title: this.options.popupTitle,
+                    buttons: false,
+                    clickableOverlay: false,
+                    modalClass : 'customer-popup'
+                };
+
+            modal(authentication_options, this.element);
+
+            // Show the login form in a popup when clicking on the sign in text
+            $('body').on('click', '.customer-login-link, '+self.options.prevLogin, function() {
+                $(self.options.register).modal('closeModal');
+                $(self.options.login).modal('openModal');
+                return false;
+            });
+
+            // Show the registration form in a popup when clicking on the create an account text
+            $('body').on('click', '.customer-register-link, '+self.options.nextRegister, function() {
+                $(self.options.login).modal('closeModal');
+                $(self.options.register).modal('openModal');
+                return false;
+            });
+
+            this.ajaxSubmit();
+        },
+
+        /**
+         * Submit data by Ajax
+         * @private
+         */
+        ajaxSubmit: function() {
+            var self = this,
+                form = this.element.find('form'),
+                inputElement = form.find('input');
+
+            inputElement.keyup(function (e) {
+                self.element.find('.messages').html('');
+            });
+
+            form.submit(function (e) {
+                if (form.validation('isValid')) {
+                    if (form.hasClass('form-create-account')) {
+                        $.ajax({
+                            url: $(e.target).attr('action'),
+                            data: $(e.target).serializeArray(),
+                            showLoader: true,
+                            type: 'POST',
+                            dataType: 'json',
+                            success: function (response) {
+                                self.showResponse(response, form.find('input[name="redirect_url"]').val());
+                            },
+                            error: function() {
+                                self.showFailingMessage();
+                            }
+                        });
+                    } else {
+                        var submitData = {},
+                            formDataArray = $(e.target).serializeArray();
+                        formDataArray.forEach(function (entry) {
+                            submitData[entry.name] = entry.value;
+                        });
+                        $('body').loader().loader('show');
+                        storage.post(
+                            $(e.target).attr('action'),
+                            JSON.stringify(submitData)
+                        ).done(function (response) {
+                            $('body').loader().loader('hide');
+                            self.showResponse(response, form.find('input[name="redirect_url"]').val());
+                        }).fail(function () {
+                            $('body').loader().loader('hide');
+                            self.showFailingMessage();
+                        });
+                    }
+                }
+                return false;
+            });
+        },
+
+        /**
+         * Display messages on the screen
+         * @private
+         */
+        displayMessages: function(className, message) {
+            $('<div class="message '+className+'"><div>'+message+'</div></div>').appendTo(this.element.find('.messages'));
+        },
+
+        /**
+         * Showing response results
+         * @private
+         * @param {Object} response
+         * @param {String} locationHref
+         */
+        showResponse: function(response, locationHref) {
+            var self = this,
+                timeout = 800;
+            this.element.find('.messages').html('');
+            if (response.errors) {
+                this.displayMessages('message-error error', response.message);
+            } else {
+                this.displayMessages('message-success success', response.message);
+            }
+            this.element.find('.messages .message').show();
+            setTimeout(function() {
+                if (!response.errors) {
+                    self.element.modal('closeModal');
+                    window.location.href = locationHref;
+                }
+            }, timeout);
+        },
+
+        /**
+         * Show the failing message
+         * @private
+         */
+        showFailingMessage: function() {
+            this.element.find('.messages').html('');
+            this.displayMessages('message-error error', $t('An error occurred, please try again later.'));
+            this.element.find('.messages .message').show();
+        }
+    });
+
+    return $.trainingankit.customerAuthenticationPopup;
+});
