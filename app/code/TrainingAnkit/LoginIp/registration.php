a10df90f50906d34447d19c9ee2cb632296b26e7
ankitkrish <ankit.shrivastava@krishtechnolabs.com>
2020-09-29 16:11:08 +0530

    ip address task

---
+++ app/code/TrainingAnkit/LoginIp/registration.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * TrainingAnkit
+ * Copyright (C) 2019 TrainingAnkit <info@training.com>
+ *
+ * NOTICE OF LICENSE
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://opensource.org/licenses/gpl-3.0.html.
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    TrainingAnkit
+ * @package     TrainingAnkit_CustomerAccount
+ * @copyright   Copyright (c) 2012 Training (http://www.training.com/)
+ * @license     http://www.TrainingAnkit.com/license-agreement.html
+ * @author      Training <info@Training.com>
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'TrainingAnkit_Loginip',
+    __DIR__
+);
