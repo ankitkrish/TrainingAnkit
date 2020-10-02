bbe18bd2fc3f412416985efca3b281d23d017f92
ankitkrish <ankit.shrivastava@krishtechnolabs.com>
2020-10-03 01:47:48 +0530

    PRoduct assign using cron tab

--- app/code/TrainingAnkit/AssignProduct/Cron/Test.php
+++ app/code/TrainingAnkit/AssignProduct/Cron/Test.php
@@ -3,26 +3,28 @@ namespace TrainingAnkit\AssignProduct\Cron;
 
 class Test
 {
-	public function execute()
-	{
-		//Get Last three day latest product
-		    $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-		    $productCollectionFactory = $objectManager->get('\Magento\Catalog\Model\ResourceModel\Product\CollectionFactory');
-		    $to = date("Y-m-d h:i:s"); // current date
-		    $from = strtotime('-3 day', strtotime($to));
-		    $from = date('Y-m-d h:i:s', $from);
-		    $collection = $productCollectionFactory->create();
-			$collection->addAttributeToSelect('*');
-			$collection->addFieldToFilter('created_at', array('from'=>$from, 'to'=>$to))->addAttributeToSort('created_at', 'DESC');
-			$category_ids = array('7');
-			$CategoryLinkRepository = $objectManager->get('\Magento\Catalog\Api\CategoryLinkManagementInterface');
-			$i = 0;
-			foreach($collection as $product){
-				if($i<=9){
-	                $CategoryLinkRepository->assignProductToCategories($product->getSku(), $category_ids);
-	                 echo $i++;
-	            }
+    public function execute()
+    {
+        //Get Last three day latest product
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $productCollectionFactory =
+        $objectManager->get('\Magento\Catalog\Model\ResourceModel\Product\CollectionFactory');
+        $to = date("Y-m-d h:i:s"); // current date
+        $from = strtotime('-3 day', strtotime($to));
+        $from = date('Y-m-d h:i:s', $from);
+        $collection = $productCollectionFactory->create();
+        $collection->addAttributeToSelect('*');
+        $collection->addFieldToFilter('created_at', ['from'=>$from, 'to'=>$to]);
+        $collection->addAttributeToSort('created_at', 'DESC');
+        $category_ids = ['7'];
+        $CategoryLinkRepository = $objectManager->get('\Magento\Catalog\Api\CategoryLinkManagementInterface');
+        $i = 0;
+        foreach ($collection as $product) {
+            if ($i<=9) {
+                $CategoryLinkRepository->assignProductToCategories($product->getSku(), $category_ids);
+                $i++;
+            }
                 
-        	} 
-	}
+        }
+    }
 }
